31af113689e6b03ed303f93648791f2b587fe69f
ucok23 <ulubis98@gmail.com>
2022-02-05 12:05:57 +0700

    refactoring jumlah ayah

--- quiz/jumlahAyah.js
+++ quiz/jumlahAyah.js
@@ -1,13 +1,16 @@
 const { quran, randomSurah} = require('../lib/util');
-
+ 
 const jmlAyah = (noOfSurah) => {
-    return quran[noOfSurah].numberOfVerses
+    return quran[noOfSurah - 1].numberOfVerses
 }
 
 class JumlahAyah {
     static quiz(req, res) {
+        let rand = randomSurah();
+        
         return res.status(200).send({
-            code: 200
+            code: jmlAyah(1),
+            message: this.foo()
         });
     }
 }
