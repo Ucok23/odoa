d9a5d3f5f82f89c3a24130935b10ef6ec2d34d8b
ucok23 <ulubis98@gmail.com>
2022-02-05 12:05:33 +0700

    adding ayah model

---
+++ models/ayah.js
@@ -0,0 +1,11 @@
+class Ayah {
+    constructor(nameOfSurah, numberOfSurah, totalAyah, numberOfAyah, ayah) {
+        this.nameOfSurah = nameOfSurah;
+        this.numberOfSurah = numberOfSurah;
+        this.totalAyah = totalAyah
+        this.numberOfAyah = numberOfAyah;
+        this.ayah = ayah
+    }
+}
+
+module.exports = Ayah
