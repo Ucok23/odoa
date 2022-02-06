712976a0930bdaefceaa2b3ef97a4bac8f20592e
ucok23 <ulubis98@gmail.com>
2022-02-06 08:31:08 +0700

    refactor surahRandomizer

--- surahRandomizer.js
+++ surahRandomizer.js
@@ -1,5 +1,6 @@
 const Ayah = require('./models/ayah');
 const getRandAyah = require('./lib/_ayahUtil')
+const juzData = require('./lib/_juzResolver')
 
 class SurahRandomizer {
     static getRandomAyahAllSurah(req, res) {
@@ -10,6 +11,28 @@ class SurahRandomizer {
             message: 'Success getting random ayah from all surah',
             data: data
         })
+    }
+
+    static getJuz(req, res) {
+        const { juz } = req.params;
+        const data = juzData(parseInt(juz));
+
+        if (juz < 1 || juz > 30) {
+            return res.status(404).send({
+                code: 404,
+                status: 'Not Found.',
+                message: `Surah "${surah}" is not found.`,
+                data: {}
+            })
+        }
+
+        return res.status(200).send({
+            code: 200,
+            status: 'OK.',
+            message: 'Success fetching juz.',
+            data
+        })
+
     }
 }
 
